6747e165bbfa185eef1c3d9ad54403cc954887b0
britto <jose@britto.tech>
2025-08-31 10:38:34 -0400

    Basic landing page

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Britto</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+<body>
+    <?php include_once('header.php'); ?>
+
+    <main>
+<!--        <h1>Bonjour, Stranger <span class="emoji"> &#128075; </span></h1>
+-->
+        <h1>Welcome to <span class="colored">Britto's</span> tiny space on the interwebs</h1>
+        <!--<p>I create beautiful software designed for humans</p>-->
+        <p>I specialize in creating hand-crafted software experiences for humans</p>
+    </main>
+</body>
+</html>
