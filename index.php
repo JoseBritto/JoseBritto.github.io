19c565267bcdc478253a68776bf896b7c928fee5
britto <jose@britto.tech>
2025-08-31 14:08:49 -0400

    Main section done

--- index.php
+++ index.php
@@ -16,7 +16,10 @@
 -->
         <h1>Welcome to <span class="colored">Britto's</span> tiny space on the interwebs</h1>
         <!--<p>I create beautiful software designed for humans</p>-->
-        <p>I specialize in creating hand-crafted software experiences for humans</p>
+        <span class="text">A fancy place where I have to show off how I'm apparently better than everyone else</span>
+
+        <button class="explore">
+            <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="24px"><path d="M440-800v487L216-537l-56 57 320 320 320-320-56-57-224 224v-487h-80Z"/></svg></button>
     </main>
 </body>
 </html>
