1d4656db8922e29cfb0f8f8863673764112312ff
britto <jose@britto.tech>
2025-08-31 22:49:23 -0400

    Main section - cleanup

--- index.php
+++ index.php
@@ -3,23 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Britto</title>
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link rel="stylesheet" href="assets/css/index.css">
 </head>
 <body>
     <?php include_once('header.php'); ?>
-
-    <main>
-<!--        <h1>Bonjour, Stranger <span class="emoji"> &#128075; </span></h1>
--->
-        <h1>Welcome to <span class="colored">Britto's</span> tiny space on the interwebs</h1>
-        <!--<p>I create beautiful software designed for humans</p>-->
-        <span class="text">A fancy place where I have to show off how I'm apparently better than everyone else</span>
-
-        <button class="explore">
-            <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="24px"><path d="M440-800v487L216-537l-56 57 320 320 320-320-56-57-224 224v-487h-80Z"/></svg></button>
-    </main>
+    <?php include_once('main.php'); ?>
 </body>
 </html>
---
+++ main.php
@@ -0,0 +1,7 @@
+<main>
+    <h1>Welcome to <span class="colored-container"><span class="colored">Britto's</span></span> tiny space on the interwebs</h1>
+    <span class="text">A fancy place to shamelessly show off my skills.</span>
+    <button class="explore">
+        <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="24px"><path d="M440-800v487L216-537l-56 57 320 320 320-320-56-57-224 224v-487h-80Z"/></svg>
+    </button>
+</main>
